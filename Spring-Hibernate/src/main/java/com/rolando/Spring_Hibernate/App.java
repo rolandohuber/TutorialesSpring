d9a1218f03578e225986ec98638ada3a63daa968
rolandohuber <rolandohuber@hotmail.com>
2014-08-16 15:59:22 -0300

    Se creo el tutorial Spring + Hibernate

---
+++ Spring-Hibernate/src/main/java/com/rolando/Spring_Hibernate/App.java
@@ -0,0 +1,13 @@
+package com.rolando.Spring_Hibernate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
